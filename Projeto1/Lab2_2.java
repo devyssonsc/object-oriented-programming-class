3386953807adef4948d8250afcb35c42dfc040e9
Devysson Cardoso <devyssonsc@gmail.com>
2024-03-06 00:59:42 +0000

    Lab2_2 checked

--- Projeto1/Lab2_2.java
+++ Projeto1/Lab2_2.java
@@ -13,6 +13,10 @@ public class Lab2_2
         Scanner input = new Scanner(System.in);
         int[][] tabela = new int[4][5];
         int defeituosas;
+        int[] somaLinhas = new int[4];
+        int[] somaDias = new int[5];
+        float[] mediaLinhas = new float[4];
+        float[] mediaDias = new float[5];
         
         for (int row = 0; row < 4; row++){
             for (int col = 0; col < 5; col++){
@@ -28,5 +32,49 @@ public class Lab2_2
             }
             System.out.print("\n");
         }
-    }
-}
+        
+        for (int row = 0; row < 4; row++){
+            for (int col = 0; col < 5; col++){
+                somaLinhas[row] += tabela[row][col];
+                somaDias[col] += tabela[row][col];
+            }
+        }
+        
+        System.out.println("\nSomas:");
+        for (int i = 0; i < 4; i++){
+            System.out.println(somaLinhas[i]);
+        }
+        System.out.println("-----------------------");
+        for (int i = 0; i < 5; i++){
+            System.out.print(somaDias[i] + " ");
+        }
+        
+        
+        for (int i = 0; i < 5; i++){
+            mediaDias[i] = (float) somaDias[i]/4;
+            if (i < 4){
+                mediaLinhas[i] = (float) somaLinhas[i]/5;
+            }
+        }
+        
+        System.out.println("\n\nMédias:");
+        for (int i = 0; i < 4; i++){
+            System.out.println(mediaLinhas[i]);
+        }
+        System.out.println("-----------------------");
+        for (int i = 0; i < 5; i++){
+            System.out.print(mediaDias[i] + " ");
+        }
+        
+        int maisDefeituosa = 0;
+        int aux = 0;
+        for (int i = 0; i < 4; i++){
+            if (somaLinhas[i] > aux){
+                maisDefeituosa = i;
+                aux = somaLinhas[i];
+            }
+        }
+        
+        System.out.print("\nLinha que mais produz peças defeituosas: Linha " + (maisDefeituosa+1));
+    }// fim do método
+}// fim da classe
