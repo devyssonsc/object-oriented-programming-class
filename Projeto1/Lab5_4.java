84083c95bb8220d581503e52ece35d78c85dec5e
Devysson Cardoso <devyssonsc@gmail.com>
2024-03-08 17:42:39 +0000

    Create Lab5_4.java

---
+++ Projeto1/Lab5_4.java
@@ -0,0 +1,43 @@
+
+/**
+ * Escreva uma descrição da classe Lab5_4 aqui.
+ * 
+ * @author Devysson S. Cardoso 
+ * @version 08/03/2024
+ */
+public class Lab5_4
+{
+    public static void main(String[] args)
+    {
+        imprimePrimos(45);
+    }//fim do main
+    
+    public static void imprimePrimos (int n)
+    {
+        for(int i = 1; i <= n; i++){
+            if (ePrimo(i)){
+                System.out.println(i);
+            }
+        }
+    }
+    
+    public static boolean ePrimo(int num)
+    {
+        if (num <= 1){
+            return false;
+        } else if (num == 2){
+            return true;
+        } else if (num % 2 == 0){
+            return false;
+        }else{
+            double raiz = Math.sqrt(num);
+            for (int i = 2; i <= raiz; i++){
+                if (num % i == 0){
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+}//fim da classe
