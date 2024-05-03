89b00c7693e6c6b09ad98d6386b3e4a059ef1aa7
Devysson Cardoso <devyssonsc@gmail.com>
2024-05-03 13:38:26 -0300

    Update Voleibol.java

--- Projetos com Eclipse/Lab11_2/src/clube/Voleibol.java
+++ Projetos com Eclipse/Lab11_2/src/clube/Voleibol.java
@@ -6,6 +6,29 @@ package clube;
 /**
  * 
  */
-public class Voleibol {
+public class Voleibol extends Modalidade {
+	private int maxJogadores;
 
+	/**
+	 * @param nome
+	 * @param escalao
+	 */
+	public Voleibol(String nome, String escalao, int maxJogadores) {
+		super(nome, escalao);
+		this.maxJogadores = maxJogadores;
+	}
+
+	/**
+	 * @return the maxJogadores
+	 */
+	public int getMaxJogadores() {
+		return maxJogadores;
+	}
+
+	@Override
+	public String toString() {
+		return "Voleibol " + super.toString() + " [maxJogadores=" + maxJogadores + "]";
+	}
+	
+	
 }
